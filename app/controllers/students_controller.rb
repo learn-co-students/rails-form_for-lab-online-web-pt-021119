0fbe21334fcf48a2f0cbebfe920e4be6859a3e98
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-07-03 12:18:53 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,23 +1,31 @@
+require 'pry'
+
 class StudentsController < ApplicationController
 
-  def new
+  def index
+    @students = Student.all
+  end
 
+  def new
+    @student = Student.new
   end
 
   def create
-
+    @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
   end
 
   def show
-
+    @student = Student.find(params[:id])
   end
 
   def edit
-
+    @student = Student.find(params[:id])
   end
 
   def update
-
+    @student = Student.find(params[:id])
+    @student = Student.update(params.require(:student).permit(:first_name, :last_name))
   end
 
 end
